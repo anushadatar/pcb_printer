32ebffb07145d64e0deef9ddc2080520888facec
Anusha Datar <anushadatar@gmail.com>
2018-11-02 10:40:10 -0400

    a script?

--- software/flatcam_scripts/gbr_to_gcode.tcl
+++ software/flatcam_scripts/gbr_to_gcode.tcl
@@ -1,3 +1,4 @@
-new
-open_gerber "edge_test.gbr"
-isolate "edge_test.gbr"
+open_gerber edge_test.gbr
+isolate edge_test.gbr -outname geometry.gbr_iso -combine
+cncjob geometry.gbr_iso -outname cnc.gbr_iso_cnc
+write_gcode cnc.gbr_iso_cnc output.gcode
