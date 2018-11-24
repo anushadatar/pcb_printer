717efa074a40a51215be0c4426b84bad8b284433
Anusha Datar <anushadatar@gmail.com>
2018-11-24 02:20:24 -0500

    literally added a comment to pi_run. TODO check that it works.

--- software/pi_run.sh
+++ software/pi_run.sh
@@ -6,6 +6,7 @@
 python3 create_shellfile.py --file $1 --tool-diameter $2 
 
 # Run the Flatcam software to create the gcode file.
+# Change paths as necessary for individual device configurations.
 python2 ../../FlatCAM-8.5/FlatCAM.py --shellfile=/home/pi/pcb_printer/software/create_gcode.tcl
 
 # Format the gcode to be more compatible with the parser.
