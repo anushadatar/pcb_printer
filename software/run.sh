ccf5a6f4f9d32ff575a98d75f9673163c416d8dc
Anusha Datar <anushadatar@gmail.com>
2018-11-23 23:52:30 -0500

    Added SVG file support.

--- software/run.sh
+++ software/run.sh
@@ -1,11 +1,15 @@
 ### Script to automate mill operation.
-# Argument 0 should be file path.
-# Argument 1 should be tool diameter (in inches). 
+# Argument 1 should be file path.
+# Argument 2 should be tool diameter (in inches). 
 
+# Crete an appropriate tcl file for the given constraints.
 python3 create_shellfile.py --file $1 --tool-diameter $2 
 
+# Run the Flatcam software to create the gcode file.
 python2 ../../FlatCAM-8.5/FlatCAM.py --shellfile=~/pcb_printer/software/create_gcode.tcl
 
+# Format the gcode to be more compatible with the parser.
 python gcode_format.py -p current_job.gcode
 
+# Stream the gcode to the arduino.
 python gcode_stream.py -s -p output.gcode
