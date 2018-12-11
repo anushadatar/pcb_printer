99b4f1c38df5dfa8c04a84ba2f33b8b049b0a85a
Anusha Datar <anushadatar@gmail.com>
2018-12-10 22:37:24 -0500

    Github messenger

--- software/create_gcode.tcl
+++ software/create_gcode.tcl
@@ -1,4 +1,4 @@
-open_gerber /home/anusha/pcb_printer/software/test_data/name_tag/name_tag-F.SilkS.gbr
-isolate name_tag-F.SilkS.gbr -dia 0.015 -outname geometry.gbr_iso -combine
+open_gerber /home/anusha/pcb_printer/software/test_data/L/L-F.SilkS.gbr
+isolate L-F.SilkS.gbr -dia 0.015 -outname geometry.gbr_iso -combine
 cncjob geometry.gbr_iso -tooldia 0.015 -outname cnc.gbr_iso_cnc
 write_gcode cnc.gbr_iso_cnc ../pcb_printer/software/current_job.gcode
