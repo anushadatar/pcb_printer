2c9fec4f821e607b4d65c0e2acb2215d4f662a09
Anusha Datar <anushadatar@gmail.com>
2018-12-10 21:31:17 -0500

    look at output.gcode

--- software/create_gcode.tcl
+++ software/create_gcode.tcl
@@ -1,4 +1,4 @@
 open_gerber /home/anusha/pcb_printer/software/test_data/name_tag/name_tag-F.SilkS.gbr
-isolate name_tag-F.SilkS.gbr -dia .0039 -outname geometry.gbr_iso -combine
-cncjob geometry.gbr_iso -tooldia .0039 -outname cnc.gbr_iso_cnc
+isolate name_tag-F.SilkS.gbr -dia 0.015 -outname geometry.gbr_iso -combine
+cncjob geometry.gbr_iso -tooldia 0.015 -outname cnc.gbr_iso_cnc
 write_gcode cnc.gbr_iso_cnc ../pcb_printer/software/current_job.gcode
