14e9c44dbfd16d5b15f118414ae70a358d0313a2
Anusha Datar <anushadatar@gmail.com>
2018-12-12 16:29:16 -0500

    added gcode tests

--- software/create_gcode.tcl
+++ software/create_gcode.tcl
@@ -1,4 +1,4 @@
 open_gerber /home/anusha/pcb_printer/software/test_data/L/L-F.SilkS.gbr
-isolate L-F.SilkS.gbr -dia 0.015 -outname geometry.gbr_iso -combine
-cncjob geometry.gbr_iso -tooldia 0.015 -outname cnc.gbr_iso_cnc
+isolate L-F.SilkS.gbr -dia 0.0039 -outname geometry.gbr_iso -combine
+cncjob geometry.gbr_iso -tooldia 0.0039 -outname cnc.gbr_iso_cnc
 write_gcode cnc.gbr_iso_cnc ../pcb_printer/software/current_job.gcode
